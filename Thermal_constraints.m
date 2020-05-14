17f33329b596a46e3d7014eeaa2fdd01bc27ffb1
Ruike Lyu <47710448+Rick10119@users.noreply.github.com>
2020-05-14 09:42:18 +0800

    爬坡的时间不是收尾闭合的

--- Thermal_constraints.m
+++ Thermal_constraints.m
@@ -1,28 +1,35 @@
-% ��ԭ�е�Լ���ϣ������ȵ糧��Լ��
+% 在原有的约束上，加上热电厂的约束
 
-%%  �ȵ糧����
+%%  热电厂数量
 % % NUMOFTHERMAL = 4;
 
 %%
-%����Լ��(WM)
+%出力约束(WM)
 P_u_min = [20, 20, 25, 25];
 P_u_max = [130, 130, 162, 162];
-%����Լ��(MW/min)*��min)
-dt = 15;                                                                    %����Ϊ15min
+%爬坡约束(MW/min)*（min)
+dt = 15;                                                                    %步长为15min
 R_u_dM = dt * [ -1.5, -3.0, -1.5, -1.8];
 R_u_uM = dt * [ 1.5, 3.0, 1.5, 1.8];
-%������top���Ѿ����壩
-% P_u = sdpvar(TIME,NUMOFTHERMAL,'full');                                     %�ȵ糧���� MW
-% R_u = zeros(TIME,1);                                                        %�ȵ糧�������� MW/min
+%出力（top中已经定义）
+% P_u = sdpvar(TIME,NUMOFTHERMAL,'full');                                     %热电厂出力 MW
+% R_u = zeros(TIME,1);                                                        %热电厂出力爬坡 MW/min
 
-%���ºͳ����Ĺ�ϵ
+%爬坡和出力的关系
 R_u = P_u - [P_u(TIME,:); P_u(1:TIME-1,:)];
 
 %%
-
+Constraints2 = [];
+% 出力约束
 for i = 1:NUMOFTHERMAL
-    for k=1:TIME
-        Constraints = [Constraints, P_u_min(i) <= P_u(k,i) <= P_u_max(i)];
-        Constraints = [Constraints, R_u_dM(i)  <= R_u(k,i) <= R_u_uM(i)];
+    for k = 1:TIME
+        Constraints2 = [Constraints2, P_u_min(i) <= P_u(k,i) <= P_u_max(i)];
+    end
+end
+
+% 爬坡约束
+for i = 1:NUMOFTHERMAL
+    for k = 2:TIME %从第二个时刻开始计算约束，因为时间不是首尾相连的
+        Constraints2 = [Constraints2, R_u_dM(i)  <= R_u(k,i) <= R_u_uM(i)];
     end
 end
